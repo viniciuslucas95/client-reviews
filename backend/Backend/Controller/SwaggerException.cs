b5d815226fd8325abb856e1918e892d8de7a5c19
Vinícius Lucas <viniciuslucas1564@gmail.com>
2023-08-20 16:10:47 -0300

    Added SwaggerException for Swagger example

---
+++ backend/Backend/Controller/SwaggerException.cs
@@ -0,0 +1,10 @@
+﻿namespace Backend.Controller;
+
+public class SwaggerException
+{
+#pragma warning disable CS8618
+    public string Name { get; }
+    public string Message { get; }
+    public int StatusCode { get; }
+#pragma warning restore CS8618
+}
