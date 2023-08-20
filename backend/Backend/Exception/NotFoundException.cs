0c9389d3ba4aab31bb75619cdc33339ab5e4e09f
Vinícius Lucas <viniciuslucas1564@gmail.com>
2023-08-20 16:10:27 -0300

    Added NotFoundException

---
+++ backend/Backend/Exception/NotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace Backend.Exception;
+
+public class NotFoundException : ExceptionBase
+{
+    public NotFoundException(string name, string message) : base(name, message, StatusCodes.Status404NotFound)
+    {
+
+    }
+}
