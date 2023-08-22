257ef122d7e185ef868835ceeaeb3122e4aed423
Vinícius Lucas <viniciuslucas1564@gmail.com>
2023-08-21 23:06:25 -0300

    Added desc sort into get paginated client reviews

--- backend/Backend/Util/QueryBuilder/ClientReview/ClientReviewQueryBuilderSqlServer.cs
+++ backend/Backend/Util/QueryBuilder/ClientReview/ClientReviewQueryBuilderSqlServer.cs
@@ -9,7 +9,7 @@ public class ClientReviewQueryBuilderSqlServer : IClientReviewQueryBuilder
 
     public string BuildPaginateSql()
     {
-        return "WITH GroupedReviews AS (SELECT date AS Date, COUNT(*) as Total, SUM(CASE WHEN score >= 9 THEN 1 ELSE 0 END) AS Promotores, SUM(CASE WHEN score >= 7 AND score < 9 THEN 1 ELSE 0 END) AS Neutros, SUM(CASE WHEN score <= 7 THEN 1 ELSE 0 END) AS Detratores FROM client_reviews GROUP BY Date) SELECT Date, Total, CASE WHEN CAST((Promotores - Detratores) AS DECIMAL(3, 2)) / Total * 100 < 0 THEN 0 ELSE CAST((Promotores - Detratores) AS DECIMAL(3, 2)) / Total * 100 END AS NPS FROM GroupedReviews ORDER BY Date OFFSET @Offset ROWS FETCH NEXT 10 ROWS ONLY;";
+        return "WITH GroupedReviews AS (SELECT date AS Date, COUNT(*) as Total, SUM(CASE WHEN score >= 9 THEN 1 ELSE 0 END) AS Promotores, SUM(CASE WHEN score >= 7 AND score < 9 THEN 1 ELSE 0 END) AS Neutros, SUM(CASE WHEN score <= 7 THEN 1 ELSE 0 END) AS Detratores FROM client_reviews GROUP BY Date) SELECT Date, Total, CASE WHEN CAST((Promotores - Detratores) AS DECIMAL(3, 2)) / Total * 100 < 0 THEN 0 ELSE CAST((Promotores - Detratores) AS DECIMAL(3, 2)) / Total * 100 END AS NPS FROM GroupedReviews ORDER BY Date DESC OFFSET @Offset ROWS FETCH NEXT 10 ROWS ONLY;";
     }
 
     public string BuildCountSql()
