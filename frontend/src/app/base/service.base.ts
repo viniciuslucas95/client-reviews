f383c938b476d7fa754633f3b136b485301d20fb
Vinícius Lucas <viniciuslucas1564@gmail.com>
2023-08-22 16:15:29 -0300

    Changed base service port in base url

--- frontend/src/app/base/service.base.ts
+++ frontend/src/app/base/service.base.ts
@@ -5,7 +5,7 @@ export default abstract class ServiceBase{
     protected readonly httpClient:HttpClient
 
     protected constructor(route:string, httpClient:HttpClient) {
-        this.baseUrl = `http://localhost:5189/api/${route}`
+        this.baseUrl = `http://localhost:3000/api/${route}`
         this.httpClient = httpClient
     }
 }
