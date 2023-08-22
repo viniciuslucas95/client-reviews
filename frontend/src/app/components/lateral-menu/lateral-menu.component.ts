362aea8ddb3a298337e743aa2ed755baf507b2bc
Vinícius Lucas <viniciuslucas1564@gmail.com>
2023-08-22 16:06:31 -0300

    Modified colors and added logo

--- frontend/src/app/components/lateral-menu/lateral-menu.component.ts
+++ frontend/src/app/components/lateral-menu/lateral-menu.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {NavigationEnd, Router} from "@angular/router";
+import {filter} from "rxjs";
 
 @Component({
   selector: 'app-lateral-menu',
@@ -6,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./lateral-menu.component.scss']
 })
 export class LateralMenuComponent {
+  public activeRoute = 'clientes'
+  public readonly defaultButtonClass = "btn btn-lg btn-primary btn-lateral-menu b-800"
 
+  constructor(router: Router) {
+    router.events
+        .pipe(
+            filter(event => event instanceof NavigationEnd)
+        )
+        .subscribe(_ => {
+          if(location.pathname.includes('clientes')){
+            this.activeRoute = 'clientes'
+          }else{
+            this.activeRoute = 'avaliacoes'
+          }
+        })
+  }
 }
